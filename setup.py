14be73539db36452956259c843b6ed359fc53ae2
ryoheinamiki515 <rnamiki2@illinois.edu>
2020-08-01 10:26:59 -0500

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using NLP to cluster text together. Uses Wikipedia articles in example',
+    author='Ryohei',
+    license='',
+)
