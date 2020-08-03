8cb384a5e3b78394c36f963a59f3f9cae1d673ae
ryoheinamiki515 <rnamiki2@illinois.edu>
2020-08-03 01:31:54 -0500

    finished rough draft of example_tests notebook

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,9 +2,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.cluster import KMeans
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
-from sklearn.metrics import silhouette_score
-import numpy as np
-import pandas as pd
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
+from scipy.stats import gmean, hmean
+
 
 def hyperopt_data(param_space, data, num_eval):
     def objective_function(params):
@@ -14,8 +14,12 @@ def hyperopt_data(param_space, data, num_eval):
         ])
         pipe.set_params(**params)
         pipe.fit(data)
-        score = silhouette_score(data, pipe.labels_)
-        return {'loss': -score, 'status': STATUS_OK}
+        silhouette = (silhouette_score(pipe["tfidf"].transform(data).toarray(), pipe["kmeans"].labels_)+1)/2
+        davies_bouldin = 1-davies_bouldin_score(pipe["tfidf"].transform(data).toarray(), pipe["kmeans"].labels_)
+        if davies_bouldin < 0:
+            davies_bouldin = 0
+        score = hmean([silhouette, davies_bouldin])
+        return {'loss': -silhouette, 'status': STATUS_OK}
     trials = Trials()
     best_param = fmin(
         objective_function,
@@ -26,23 +30,3 @@ def hyperopt_data(param_space, data, num_eval):
     )
     return trials, best_param
 
-
-def train_model(data, labels, n_clusters, show_results=False, changed_to_original=None):
-    vectorizer = TfidfVectorizer(max_df=0.8, min_df=0.2)
-    vectors = vectorizer.fit_transform(data)
-    feature_names = vectorizer.get_feature_names()
-    dense = vectors.todense()
-    denselist = dense.tolist()
-    df = pd.DataFrame(denselist, columns=feature_names)
-    km = KMeans(n_clusters=n_clusters)
-    km.fit(vectors)
-    if show_results:
-        centers_ordered = km.cluster_centers_.argsort()[:, ::-1]
-        for i in range(n_clusters):
-            indices = np.where(km.labels_ == i)[0]
-            cluster_labels = [labels[i] for i in indices]
-            print(f"Cluster #{i}:", ", ".join(cluster_labels))
-            prominent_words = changed_to_original.loc[df.columns[centers_ordered[i][:5]]].unique()
-            print("Prominent words:", ", ".join(prominent_words))
-            print("==============================\n")
-    return km
